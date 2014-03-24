f20bfc42fac5ae99edaec20fea82536690be6890
André Gonçalves <andreslb1@gmail.com>
2014-03-24 14:59:38 +0000

    Created app italent with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Italentapp::Application.config.secret_key_base = '13d262afbfaeed256ed75def2b38ea0d13ffd694457eaee9b709d70e9baec5579f81a066e2025ef81ce6f14ff1747fc45feb87df3c8031aa054ad0d4ac0b0ede'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Italentapp::Application.config.session_store :cookie_store, key: '_italentapp_session'
