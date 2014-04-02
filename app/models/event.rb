bfad6fc93d5d71c358dd4a8203209c20b58a84cf
Goncalo Oliveira <goncalo.palaio@gmail.com>
2014-04-03 00:01:28 +0100

    event model with validations and spec

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+	validates :title,presence:true, length: { maximum:100 }
+	validates :description,presence:true, length: { maximum:255 }
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before do
+        @event = FactoryGirl.build_stubbed(:event)
+    end
+
+    subject { @event } 
+
+	it { should respond_to :title }
+	it { should respond_to :description }
+	it { should respond_to :location }
+	it { should respond_to :date_limit }
+	it { should respond_to :num_attendings }
+	it { should respond_to :num_invitations }
+	it { should respond_to :created_at }
+	it { should respond_to :updated_at }
+
+	it { should be_valid }
+
+	describe "title should not be empty" do
+		before { @event.title = "" }
+        it { should_not be_valid }
+	end
+	
+	describe "title should be short" do
+		before { @event.title = 'a' * 110 }
+    	it { should_not be_valid }
+	end
+	describe "description should be short" do
+		before { @event.description = 'a' * 265 }
+    	it { should_not be_valid }
+	end
+
+
 end
