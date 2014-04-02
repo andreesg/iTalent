e1d7b9054fb05cf59597575bf81dcb96025d13b5
Goncalo Oliveira <goncalo.palaio@gmail.com>
2014-04-02 23:46:08 +0100

    migration on dev and test

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327150021) do
+ActiveRecord::Schema.define(version: 20140402221643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "date_limit"
+    t.integer  "num_attendings"
+    t.integer  "num_invitations"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
