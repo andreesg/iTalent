7564baefe4a6e1f4d9b7bd74bc08e7a71f3d5566
goncalonsp <goncalonsp@gmail.com>
2014-04-14 18:42:54 +0100

    Adds authentication tests to the Timeline.

--- spec/controllers/timeline_controller_spec.rb
+++ spec/controllers/timeline_controller_spec.rb
@@ -2,4 +2,41 @@ require 'spec_helper'
 
 describe TimelineController do
 
+	before :all do
+		@user = create(:user)
+	end
+
+	after :all do
+		@user.destroy
+	end
+
+	describe "GET #index" do
+		
+		describe "when user is not authenticated" do
+
+			it "redirects to the sign_in page" do
+        get :index
+        response.should redirect_to new_user_session_path
+      end
+
+		end
+
+		describe "when user is authenticated" do
+
+			before :each do
+				sign_in @user
+			end
+
+			it "assigns the requested Publications to @publications"
+			it "assigns the requested Events to @events"
+
+	    it "renders the :index template" do
+	      get :index
+	      response.should render_template :index
+	    end
+
+		end
+
+	end
+
 end
