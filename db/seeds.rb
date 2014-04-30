066201f5a1ecc7ef73254afeb8b19828b45f38e4
Hugo Ribeira <hugoribeira@gmail.com>
2014-04-30 16:30:45 +0100

    Add seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+5.times do |i|
+  User.create!(
+    email: "teste#{i}@example.com",
+    password: "12345678",
+    password_confirmation: "12345678"
+  )
+end
+
+10.times do |i|
+  Tag.create!(name: "Tag ##{i}", num_subscribers: 0)
+end
+
+10.times do
+  e = Event.new(
+    title: "Evento ##{(rand*1000).to_i}",
+    description: "Lorem impsum bla "*(rand*15).ceil,
+    location: "Coimbra",
+    date_limit: Time.now + (rand*10).to_i.days,
+    date_start: Time.now + (10 + rand*10).to_i.days,
+    num_attendings: (rand*100).to_i,
+    num_invitations: (rand*100).to_i,
+  )
+
+  e.creator = User.order("random()").limit(1).to_a.first
+  e.tags << Tag.order("random()").limit(2)
+
+  e.save!
+end
+
+15.times do
+  e = Publication.new(
+    text: "Lorem impsum "*(rand*10).ceil,
+  )
+
+  e.creator = User.order("random()").limit(1).to_a.first
+  e.tags << Tag.order("random()").limit(2)
+
+  e.save!
+end
