7c96fca4f70aa8cfdaefd67d2872e6a2ecbb474f
goncalopalaio <goncalo.palaio@gmail.com>
2014-05-15 02:00:22 +0100

    added a hidden field for max_attendees

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -58,6 +58,6 @@ class EventsController < ApplicationController
   private 
   
   def event_params
-    params.require(:event).permit(:title,:description,:date_start,:date_limit,:tags)
+    params.require(:event).permit(:title,:description,:date_start,:max_attendees,:date_limit,:tags)
   end
 end
