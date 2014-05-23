f31c2dd5c8bae36b443b21b31a2005c3b5ea46cc
Joao Diogo Costa <jdscosta91@gmail.com>
2014-05-23 02:02:34 +0100

    Add access_logs to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :register_activity
+
   # checks if the user has liked the publication
   def liked_publication?(publication_id)
     return false if (publication = Publication.find(publication_id)).nil?
@@ -12,5 +14,19 @@ class ApplicationController < ActionController::Base
 
   # makes the method available for all views
   helper_method :liked_publication?
+
+  private
+
+  def register_activity
+    unless current_user.nil?
+      if current_user.access_logs.present?
+        if Time.now - current_user.access_logs.last.created_at > 10.minutes
+          current_user.access_logs.create
+        end
+      else
+        current_user.access_logs.create
+      end
+    end
+  end
 
 end
