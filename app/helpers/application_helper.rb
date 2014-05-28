96b9262633ed9ef539af32b1043afe432d6a2495
goncalonsp <goncalonsp@gmail.com>
2014-05-28 21:12:02 +0100

    Reverted the default date in the initializers. Added a helper method in application_helper.rb to convert dates to the correct format.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
-	def render_header?
-		return true if (controller_name == 'devise/registrations' and action_name == 'new') or
-			(controller_name == 'devise/registrations' and action_name == 'create') or
-			(controller_name == 'devise/sessions' and action_name == 'new') or
-			(controller_name == 'devise/sessions' and action_name == 'create')
-		return true
-	end
+  def render_header?
+    return true if (controller_name == 'devise/registrations' and action_name == 'new') or
+      (controller_name == 'devise/registrations' and action_name == 'create') or
+      (controller_name == 'devise/sessions' and action_name == 'new') or
+      (controller_name == 'devise/sessions' and action_name == 'create')
+    return true
+  end
+
+  def format_date(date)
+    return date.strftime('%d/%m/%Y %H:%M')
+  end
 end
